1b3160fee735673d925d23412572bb403093ef4c
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-09-01 13:41:11 +0100

    fix: path to owl file must include repo name

--- TestSPMResultDataModel.py
+++ TestSPMResultDataModel.py
@@ -53,7 +53,7 @@ class TestSPMResultsDataModel(unittest.TestCase, TestResultDataModel):
         self.spmexport.parse(self.spm_export_ttl, format='turtle')
 
         # Retreive owl file for NIDM-Results
-        self.owl_file = os.path.join(RELPATH, 'nidm', 'nidm', 'nidm-results', 'nidm-results.owl')
+        self.owl_file = os.path.join(RELPATH, 'nidm-results_spm', 'nidm', 'nidm', 'nidm-results', 'nidm-results.owl')
 
     def test01_class_consistency_with_owl(self):
         my_exception = check_class_names(self.spmexport, "SPM example001", owl_file=self.owl_file)
